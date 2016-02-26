7f2f6633d539481450f154d54755634edf315b17
seolhwa <ratfg31@naver.com>
2016-02-26 12:28:54 +0900

    ㅇㅇ

--- dabdab/src/main/java/bitcamp/java77/controller/Dabdab.java
+++ dabdab/src/main/java/bitcamp/java77/controller/Dabdab.java
@@ -3,7 +3,8 @@ package bitcamp.java77.controller;
 public class Dabdab {
 	public static void main(String[] args) {
 		String dabdab = "답답아~ 뭔일이니~";
-		dabdab = "그만화내나한테";
 		System.out.println("안녕");
+		dabdab = "그만화내나한테";
+		System.out.println("싫어");
 	}
 }
