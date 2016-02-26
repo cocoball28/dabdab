e270f6e027371eff91b57672494ea11aa254991e
seolhwa <ratfg31@naver.com>
2016-02-26 12:01:24 +0900

    안녕

--- dabdab/src/main/java/bitcamp/java77/controller/Dabdab.java
+++ dabdab/src/main/java/bitcamp/java77/controller/Dabdab.java
@@ -1,5 +1,8 @@
 package bitcamp.java77.controller;
 
 public class Dabdab {
-	String dabdab = "답답아~ 뭔일이니~";
+	public static void main(String[] args) {
+		String dabdab = "답답아~ 뭔일이니~";
+		System.out.println("안녕");
+	}
 }
